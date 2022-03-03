fc0972ec02d3b37b2fbdcc465ebd28848359e8b7
Iuliia El Sammak <juliag875@gmail.com>
2022-03-03 00:18:05 -0500

    commit tree 22834176a9db70d833d3f9684f04e77d9b42948e

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,18 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+  #plural
+
+  # def reviews
+  #   Review.where(game_id: self.id)
+  # end
 end
+
+
+# game = Game.first
+# Review.create(score: 10, comment: "10 stars", game: game)
+
+# game = Game.first
+# game.reviews << Review.new(score: 3, comment: "meh")
+# =
+# game = Game.first
+# game.reviews.create(score: 4, comment: "it's alright I guess")
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
 class Review < ActiveRecord::Base
+  belongs_to :game
+  # belongs_to(:game) - method takes arg of symbol(must be singular)
+
+  # a review belongs to a game
+  # def game
+  # self is the review instance
+  # Game.find(self.game_id)
   
 end
+
+# Create a review
+# review = Review.create(score: 8, comment: "wow, what a game")
+# Create a game associated with the review
+# review.create_game(title: "My favorite game")
+# Save the association
+# review.save
